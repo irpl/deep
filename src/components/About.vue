82b63b0a05574e88344cdddfb4838dff777bec3f
Phillip Logan <philo3194@gmail.com>
2020-07-16 23:15:43 -0500

    edits

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,21 +7,14 @@
       </div>
       <div class="doop">
         <h2>Ok, here we go</h2>
-        <p>From as far back as I can remember, I was the child who was more eager to find that green circuit board in my remote controlled cars than race them. I always knew I would find a home in the industry of coding and electronics.</p>
+        <p>From as far back as I can remember, I was the child who was more eager to find that green circuit board in my remote controlled cars than race them. I always knew I would find a home in the industry of Coding and Electronics.</p>
         <p>Being born and raised in Kingston, Jamaica, I found it difficult to access information about it and it was rare to encounter peers interested in the trade. There was a lack of emphasis on the importance of technology development.</p>
-        <p>To change this and enhance my passion, I graduated from the University of the West Indies with a degree in Computer Systems Engineer, where I made my future in computing and electronics concrete. And the more I learned, the more I wanted to share and help others blossom their curiosity into a potential career.</p>
-        <p>I enjoy tutoring in my spare time and I also teach introductory web development and electronics professionally on a tertiary level. At the Univeristy of the West Indies</p>
-        <p>Being an educator in a field that is continuously evolving and quickly outdated, ensuring I keep myself current is imperative to be successful. Having this professional background complements my love for learning, and enables me to enrich most of my self taught knowledge with that of other seasoned developers/makers (or whatever you call yourselves) as well.</p>
+        <p>To change this and enhance my passion, I graduated from the University of the West Indies with a degree in Computer Systems Engineering, where I made my future in computing and electronics concrete. And the more I learned, the more I wanted to share and help others blossom their curiosity into a potential career.</p>
+        <p>I enjoy tutoring in my spare time and I also teach introductory web development and electronics professionally on a tertiary level at my alma-mater.</p>
+        <p>Being an educator in a field that is continuously evolving, ensuring I keep myself current is imperative to be successful. Having this professional background complements my love for learning, and enables me to enrich most of my self taught knowledge with that of other seasoned developers/makers (or whatever you call yourselves) as well.</p>
+        <!-- <p>It encapsulates so many areas of expertise because the processes are so meticulous, like how I can recall and apply the significance of waves from St. George’s College’s Physics Class 10 years ago in JavaScript today.</p>  -->
+        <p>This is one of the things I gravitate towards the most - how we can involve technology in the things we think about the least.</p>
       </div>
-      <!-- <div class="doop">
-        <h2>Aaa little bit more about me..</h2>
-        <p>I’m an independent remote lead design engineer, author and speaker, based in Lebanon. I love the Web platform and I want to make it better for everyone. And I consider myself to be completely blessed that I get to build it for a living.</p>
-        <p>I have a Bachelor’s Degree in Computer Sciences; but everything I know of and on the Web today is self-taught. I’ve always wanted to study multiple majors because I never wanted to stop learning. I’m grateful that I ended up in a field where one of the major requirements for staying relevant is to continuously keep learning.</p>
-      </div> -->
-      <!-- <div class="doop" style="grid-column: 8 / 13"> -->
-      <!-- <img src="http://www.facetheforce.today/random/600?r=1" /> -->
-      <!-- <img src="https://picsum.photos/600" /> -->
-      <!-- </div> -->
     </section>
   </main>
 </template>
